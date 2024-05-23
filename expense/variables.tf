0cff0d9226ba7f55318caa132e98528a97977a5e
LingannaJ <jlinganna104@gmail.com>
2024-05-23 17:25:24 +0530

    terraform-multi

--- expense/variables.tf
+++ expense/variables.tf
@@ -1,15 +1,14 @@
+# ec2 variables
+variable "instance_names" {
+    type = list
+    default = ["db","backend","frontend"]
+}
 variable "image_id" {
   type        = string #optional
   default     = "ami-090252cbe067a9e58" #optional
   description = "RHEL-9 AMI ID" #optional
 }
 
-
-variable "instance_names" {
-    type = list 
-    default = ["db", "backend", "frontend"]
-
-}
 variable "instance_type" {
     default = "t3.micro"
     type = string
@@ -19,34 +18,38 @@ variable "common_tags" {
     default = {
         Project = "Expense"
         Environment = "Dev"
-        Terraform = "True"
-    
+        Terraform = "true"
     }
 }
 
+#sg variables
 variable "sg_name" {
     default = "allow_ssh"
-
 }
 
 variable "sg_description" {
     default = "allowing port 22"
-
 }
 
 variable "ssh_port" {
     default = 22
-
 }
-
 
 variable "protocol" {
     default = "tcp"
-
 }
 
-variable "allow_cidr" {
+variable "allowed_cidr" {
     type = list(string)
     default = ["0.0.0.0/0"]
+}
 
+
+#r53 variables
+variable "zone_id" {
+    default = "Z08884492QFPW45HM4UQO"
+}
+
+variable "domain_name" {
+    default = "daws78s.online"
 }
